837a83d8ab7574b44c8821046c6f9996473ab6e0
doimoi9191 <doimoi9191@noreply.github.com>
2018-04-09 23:03:08 +0700

    11h03 di ngu mai lam tiep

---
+++ Bai 10 cac loai loi trong python/LoiNghiepVu.py
@@ -0,0 +1,7 @@
+# Yêu cầu 1 đằng làm 1 nẻo
+
+t = 10 #điểm toán
+l =9 #điểm lý
+h=7.5 #điểm hóa
+dt=t+l+h/3 # điểm trung bình = toán + lý + hóa /3
+print(dt)
---
+++ Bai 10 cac loai loi trong python/LoiThucThi.py
@@ -0,0 +1,9 @@
+try:
+    x = 5
+    y=0
+    z=x/y
+    print(z)
+
+except:
+    print("Bị lỗi rồi")
+print("Cảm ơn bạn đã dùng phần mềm")
